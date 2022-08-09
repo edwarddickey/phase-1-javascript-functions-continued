1525a75b6a113fbf7e536cf8882beb4c3ee45fb4
Edward Dickey <edward.dickey@ECDs-MacBook-Pro.local>
2022-08-09 10:40:42 -0600

    commit tree ccedc78c7979b8e20208be039465372f9be1d109

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(task = "roller-skate"){
+    return`This Saturday, I want to ${task}!`;
+
+}
+let mondayWork = function(task = "go to the office") {
+    return `This Monday, I will ${task}.`
+}
+let wrapAdjective = function(style = "*") {
+    return function(adjective = "special"){
+        return `You are ${style}${adjective}${style}!`
+    }
+}
+//aturday(task
